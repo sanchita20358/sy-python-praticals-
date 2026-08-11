8955b1a7cc82ece5f334c7004a74427a53aaebc8
sanchita20358 <sanchitanikam631@gmail.com>
2026-08-11 12:52:05 +0530

    Add files via upload

---
+++ feedback formate.py
@@ -0,0 +1,12 @@
+customer_name=input("Enter customer name:")
+product_name=input("Enter product name:")
+feedback=input("Enter your feedback:")
+
+customer_name=customer_name.strip().title()
+product_name=product_name.strip().title()
+feedback=feedback.capitalize()
+
+print("==========feedback============")
+print("Customer Name:",customer_name)
+print("Product Name:",product_name)
+print("Feedback:",feedback)
